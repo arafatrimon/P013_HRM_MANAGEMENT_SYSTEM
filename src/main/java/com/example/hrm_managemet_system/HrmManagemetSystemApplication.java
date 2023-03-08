87cac94869fea85723767eef8a78812179f9fc82
arafat-rimon <rimonarafat2020@gmail.com>
2023-03-08 22:56:35 +0600

    first commit

---
+++ src/main/java/com/example/hrm_managemet_system/HrmManagemetSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.hrm_managemet_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmManagemetSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrmManagemetSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hrm_managemet_system/HrmManagemetSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hrm_managemet_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmManagemetSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
